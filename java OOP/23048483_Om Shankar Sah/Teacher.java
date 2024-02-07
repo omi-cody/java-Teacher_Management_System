412201717a8cf0d67ed0aa3f17687b1a6f1bdf7a
Om Shankar Sah <155171305+omi-cody@users.noreply.github.com>
2024-02-08 00:33:24 +0545

    Java code
    It contains all the code requied for our coursework

---
+++ java OOP/23048483_Om Shankar Sah/Teacher.java
@@ -0,0 +1,70 @@
+
+/**
+ * Write a description of class Teacher here.
+ *
+ * @author (OM SHANKAR SAH)
+ * @version (2023-01-06)
+ */
+public class Teacher//Defined a class Teacher.
+{
+    private int teacherId,workingHour;//Declare instance Variables or attributes.
+    private String teacherName,address,workingType,empStatus;
+    public Teacher(int teacherId,String teacherName,String address,
+    String workingType,String empStatus)//define a contructor method
+    {
+        this.teacherId=teacherId;//Initilizing the instance variable.
+        this.teacherName=teacherName;
+        this.address=address;
+        this.workingType=workingType;
+        this.empStatus=empStatus;
+        this.workingHour=0;//Assigned the parameter of working hour as 0 as it is not assigned yet.
+    }
+   
+    public int getTeacherId()//Define accessor method for teacher ID.
+    {
+        return this.teacherId;
+    }
+    
+    public int getWorkingHour()//Define accessor method for working Hour.
+    {
+        return this.workingHour;
+    }
+    public String getTeacherName()//Define accessor method for teacher name.
+    {
+        return this.teacherName;
+    }
+    public String getAddress()//Define accessor method for address.
+    {
+        return address;
+    }
+    public String getWorkingType()//Define accessor method for working type.
+    {
+        return this.workingType;
+    }
+    public String getEmpStatus()//Define accessor method for employment status.
+    {
+        return this.empStatus;
+    }
+    public void setWorkingHour(int workingHour)//Define setter or mutator method for working hour
+    {
+        this.workingHour=workingHour;//assigning the parameter value to working hour.
+    }
+    public void Display()//Define the display method
+    {//Print the teacherid,teacher name,address ,working type and employment status.
+        System.out.println("Teacher ID is: "+this.getTeacherId());
+        System.out.println("Teacher Name is: "+this.getTeacherName());
+        System.out.println("Address is: "+this.getAddress());
+        System.out.println("Working Type is: "+this.getWorkingType());
+        System.out.println("Employment Status: "+this.getEmpStatus());
+        if(workingHour==0)//Check if the working hour is assigned or not.
+        {
+            System.out.println("Teacher working Hour isn't assigned yet");//if not then suitable message is printed.
+        }
+        else if(workingHour>0)
+        {
+            System.out.println("Working Hour is: "+this.getWorkingHour());//if yes,working hour is printed.
+        }
+    }
+}
+
+
---
+++ java OOP/23048483_Om Shankar Sah/Tutor.java
@@ -0,0 +1,125 @@
+
+/**
+ * Write a description of class Tutor here.
+ *
+ * @author (OM SHANKAR SAH)
+ * @version (2024-01-06)
+ */
+public class Tutor extends Teacher//Define tutor subclass of Parent class Teacher
+{
+    //Declare attributes
+    private double salary;
+    private String speci,acaQual;
+    private int performIndex;
+    private boolean isCertified;
+    
+    //Define constructor of tutor class
+    public Tutor(int teacherId,String teacherName,String address,String workingType,
+    String empStatus,int workingHour,double salary,String speci,String acaQual,int performIndex)
+    {
+        //Call the teacher class constructor and setter method
+        //super(teacherId,teacherName,address,workingType,empStatus);
+        this.setWorkingHour(workingHour);
+        //Assign the salary ,specilization ,academic qualification,performance index with parameter values
+        this.salary=salary;
+        this.speci=speci;
+        this.acaQual=acaQual;
+        this.performIndex=performIndex;
+        this.isCertified=false;//Set iscertified to false
+    }
+    //Define accessor methods for each attributes
+    public double getSalary()
+    {
+        return this.salary;
+    }
+    public String getSpeci()
+    {
+        return this.speci;
+    }
+    public String getAcaQual()
+    {
+        return this.acaQual;
+    }
+    public int getPerformIndex()
+    {
+        return this.performIndex;
+    }
+    public boolean getIsCertified()
+    {
+        return this.isCertified;
+    }
+    //Define setter method for new salary
+    public void setSalary(double newSalary,int newPerformIndex)
+    {
+        //Check if the perfomance index is greater than 5 and working hour is greater than 20 hour or not.
+        if(newPerformIndex>5 && getWorkingHour()>20)
+        {
+            //Calculate the appraisal percentage according to perfomance index
+            double appraisalPer;
+            if(newPerformIndex>=5 && newPerformIndex<=7)
+            {
+                appraisalPer=0.05;            
+            }
+            else if(newPerformIndex>=8 && newPerformIndex<=9)
+            {
+                appraisalPer=0.1;
+            }
+            else
+            {
+                appraisalPer=0.2;
+            }
+            //Calculate the new salary
+            this.salary=newSalary+(appraisalPer*newSalary);
+            this.performIndex=newPerformIndex;
+            this.isCertified=true;//Set the iscertified to true
+        }
+        else
+        {
+            //If not cerified then print suitable message and set is cerified to false
+            System.out.println("The Tutor isn't Certified yet so that Salary cannot be appraised");
+            this.isCertified=false;
+        }
+        
+    }
+    public void removeTutor()//Define removetutor method
+    {
+        if(!isCertified)//Check if the tutor is certified yet or not
+        {
+            /*If not certified then set salary,perfomance to zero and specification,
+            academic qualification to null and is certified to false*/
+            this.salary=0;
+            this.speci="";
+            this.acaQual="";
+            this.performIndex=0;
+            this.isCertified=false;
+        }
+        else
+        {
+            //Print suitable message
+            System.out.println("The Tutor is Certified and Salary is Appraised");
+        }
+    }
+    //Define display method
+    public void Display()//method overridding
+    
+    {
+        //Check if the tutor is certified then print all the detail from parent class and subclass
+        if(isCertified)
+        {
+            super.Display();//Call the display method from teacher class
+            //Print salary,specification,academic qualification,performance index
+            System.out.println("The Salary of Tutor is: "+this.getSalary());
+            System.out.println("The Specilization is: "+this.getSpeci());
+            System.out.println("The Academic Qualification is: "+this.getAcaQual());
+            System.out.println("The Performance Index is:"+this.getPerformIndex());
+            
+        }
+        //If not certified then call the display method of parent class only
+        else
+        {
+            super.Display();
+        }
+    }
+
+}
+
