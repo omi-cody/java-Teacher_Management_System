3981d22b3c5a23e3e8e0d1551803b54cbb4267bc
omi-cody <omshankarsah4548@gmail.com>
2024-04-06 00:20:08 +0545

    Component name addded

--- java OOP/23048483_Om Shankar Sah/TeacherGUI.java
+++ java OOP/23048483_Om Shankar Sah/TeacherGUI.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 
 /**
  * Write a description of class TeacherGUI here.
@@ -7,5 +9,22 @@
  */
 public class TeacherGUI
 {
-    
+    private JFrame teacherGui;
+    private JPanel titlePanel,TmainPanel,salaryPanel,rmvtutorpanel,footerPanel;
+    private JLabel tTutor ,teacherId_L,teacherName_L,add_L,workingT_L,speci_L,workingH_L,empStat__L,acaQuali_L,performanceI_L,salary_L;
+    private JTextField teacherId_T,teaecherName_T,address_T,workingT_T,speci_T,workingH_T,empStat_T,acaQuali_T,performanceI_T,salary_T;
+    private JButton addTutor_Btn,setSalary_Btn,remTutor_Btn,display_Btn,clear_Btn;
+    TeacherGUI(){
+        teacherGui=new JFrame("Tutor");
+        teacherGui.setDefaultCloseOperation(teacherGui.EXIT_ON_CLOSE);
+        teacherGui.setSize(500, 530);
+        
+
+        teacherGui.setVisible(true);
+        
+    }
+    public static void main(String[] args) {
+        new TeacherGUI();
+    }
+
 }
