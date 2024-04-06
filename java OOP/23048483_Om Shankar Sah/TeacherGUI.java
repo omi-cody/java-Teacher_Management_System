2884bb54bf98a9dd87183c060698c3cc5d5f7a76
omi-cody <omshankarsah4548@gmail.com>
2024-04-07 00:44:16 +0545

    import-modified

--- java OOP/23048483_Om Shankar Sah/TeacherGUI.java
+++ java OOP/23048483_Om Shankar Sah/TeacherGUI.java
@@ -2,9 +2,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.*;
-import javax.swing.border.Border;
 
 /**
  * Write a description of class TeacherGUI here.
